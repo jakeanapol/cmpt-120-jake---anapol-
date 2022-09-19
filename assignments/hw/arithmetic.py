f34609b00297b1978bd8abd20f5bc6ff895edbda
jakeanapol <Jake.Anapol1@marist.edu>
2022-09-19 16:48:49 -0400

    Create arithmetic.py
    added a file

---
+++ assignments/hw/arithmetic.py
@@ -0,0 +1,23 @@
+def add(a, b):
+    sum=a+b
+    return sum
+
+def subtract(a, b):
+    diff=a-b
+    return diff
+
+def multiply(a, b):
+    prod=a*b
+    return
+
+def divide(a, b):
+    quotient=a/b
+    return
+
+def power(a, b):
+    exponent=a**b 
+    return 
+
+def mod(a, b):
+    Remainder=a%b
+    return
