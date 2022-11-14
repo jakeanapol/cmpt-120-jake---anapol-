0a083dfc6e18e5947dbeaf5c6b2eafe33af844bd
jakeanapol <Jake.Anapol1@marist.edu>
2022-11-14 14:07:05 -0500

    update main.py

--- assignments/api/main.py
+++ assignments/api/main.py
@@ -14,3 +14,22 @@ def hello():
 if __name__ == "__main__":
     # Runs the Flask application only if this file itself being run.
     app.run()
+    # Creating a "books" JSON / dict to emulate data coming from a database.
+books = [
+    {
+        "id": 1,
+        "title": "Harry Potter and the Goblet of Fire",
+        "author": "J.K. Rowling",
+        "isbn": "1512379298"
+    },
+    {
+        "id": 2,
+        "title": "Lord of the Flies",
+        "author": "William Golding",
+        "isbn": "0399501487"
+    }
+]
+@app.route("/books", methods=["GET"])
+def get_books():
+    # Returns the books list in JSON which stands for JavaScript Object Notation.
+    return jsonify({"books": books})
